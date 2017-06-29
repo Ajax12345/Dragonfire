5f323f2367ff6cea998a01d77ce2dce92d50f38c
Ajax1234 <pentester18@gmail.com>
2017-06-29 10:24:38 -0400

    Update dragonfire_web_search.py

--- dragonfire_web_search.py
+++ dragonfire_web_search.py
@@ -4,9 +4,19 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox(executable_path="/Users/davidpetullo/Downloads/geckodriver")
 
 
-com = "SEARCH IN WEB FOR YANKEES BASEBALL STANDINGS"
+com = "GOOGLE PICTURES OF FORD"
 if 3 > 4:
     pass
+
+elif "GOOGLE" in com and "IMAGE" in com or "PICTURES" in com or "IMAGE" in com or "IMAGES" in com:
+    useless_words = ["GOOGLE", "IMAGE", "OF", "PICTURES", "IN", "WEB", "IMAGES"]
+
+    new_com = [i.lower() for i in com.split() if i not in useless_words]
+
+
+    url = "https://www.google.com/search?q=[{}]&tbm=isch".format(' '.join(new_com))
+
+    browser.get(url)
 
 elif "GOOGLE" in com or "SEARCH" in com or "WEB" in com:
     useless_words = ["GOOGLE", "SEARCH", "WEB", "IN", "FOR"]
